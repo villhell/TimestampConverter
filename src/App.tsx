54198963b1d4e4bfa16916222ce4b644ef12466e
villhell <lilly.of.the.valley.to@gmail.com>
2023-07-15 15:38:26 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,117 @@
+import { useState } from 'react';
+import {
+  Grid,
+  Button,
+  TextField,
+  Typography,
+  Snackbar,
+  SnackbarCloseReason,
+} from '@mui/material';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import React from 'react';
+import { getTimeStamp } from './converter/TimestampConverter';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>((props, ref) => (
+  <MuiAlert elevation={6} variant="filled" ref={ref} {...props} />
+));
+
+function App() {
+  const [timestamp, setTimestamp] = useState(73548315816);
+  const [convertTimestamp, setConvertTimestamp] = useState('');
+  const [error, setError] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const changeTimestamp = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTimestamp(parseInt(e.target.value, 10));
+  };
+
+  const btnConvert_Click = () => {
+    try {
+      const convertTimestamp = getTimeStamp(timestamp, 104);
+      setConvertTimestamp(convertTimestamp);
+    } catch (e) {
+      setError('An error occurred during conversion');
+      setOpen(true);
+    }
+  };
+
+  // SnackbarのonCloseハンドラー
+  const handleSnackbarClose = (reason: SnackbarCloseReason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  // AlertのonCloseハンドラー
+  const handleAlertClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Grid
+      container
+      spacing={6}
+      direction="column"
+      alignItems="center"
+      justifyContent="flex-start"
+      marginTop={10}
+      style={{ minHeight: '100vh' }}
+    >
+      <Grid item xs={12} md={6}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Symbol Timestamp Converter
+        </Typography>
+      </Grid>
+      <Grid item xs={12} md={6} style={{ width: '40%' }}>
+        <TextField
+          id="address"
+          name="address"
+          label="Address"
+          variant="outlined"
+          required
+          fullWidth
+          onChange={changeTimestamp}
+          value={timestamp}
+          style={{ width: '100%', maxWidth: '100%' }}
+        />
+        <Grid container item justifyContent="center">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={btnConvert_Click}
+            style={{ marginTop: '10px' }}
+          >
+            Convert
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Typography variant="body1" gutterBottom>
+          Convert Timestamp:
+        </Typography>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Typography variant="h4" gutterBottom>
+          {convertTimestamp}
+        </Typography>
+      </Grid>
+
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={(_, reason) => handleSnackbarClose(reason)}
+      >
+        <Alert
+          onClose={handleAlertClose}
+          severity="error"
+          sx={{ width: '100%' }}
+        >
+          {error}
+        </Alert>
+      </Snackbar>
+    </Grid>
+  );
+}
+
+export default App;
---
+++ src/converter/TimestampConverter.ts
@@ -0,0 +1,39 @@
+export const MAIN_NET_NEMESIS_BLOCK_SYMBOL_TIME: number = 1615853185 * 1000;
+export const TEST_NET_NEMESIS_BLOCK_SYMBOL_TIME: number = 1637848847 * 1000;
+
+// timestamp
+export const getTimeStamp = (
+  timestamp: number,
+  networkType: number
+): string => {
+  let nemesisTime = 0;
+
+  if (networkType === 104) {
+    nemesisTime = MAIN_NET_NEMESIS_BLOCK_SYMBOL_TIME;
+  } else if (networkType === 152) {
+    nemesisTime = TEST_NET_NEMESIS_BLOCK_SYMBOL_TIME;
+  }
+  const datetime = new Date(timestamp + nemesisTime);
+  const formatDatetime = formatDate_yyyyMMddhhmmss(datetime);
+  console.log(formatDatetime);
+  return formatDatetime;
+};
+
+export const formatDate_yyyyMMddhhmmss = (current_datetime: Date) => {
+  const yyyy = current_datetime.getFullYear();
+  const MM = (current_datetime.getMonth() + 1).toString().padStart(2, '0');
+  const dd = current_datetime.getDate().toString().padStart(2, '0');
+  const hh = current_datetime.getHours().toString().padStart(2, '0');
+  const mm = current_datetime.getMinutes().toString().padStart(2, '0');
+  const ss = current_datetime.getSeconds().toString().padStart(2, '0');
+  const formatted_date = `${yyyy}-${MM}-${dd} ${hh}:${mm}:${ss}`;
+  return formatted_date;
+};
+
+export const formatDate_yyyyMMdd = (current_datetime: Date) => {
+  const yyyy = current_datetime.getFullYear();
+  const MM = (current_datetime.getMonth() + 1).toString().padStart(2, '0');
+  const dd = current_datetime.getDate().toString().padStart(2, '0');
+  const formatted_date = `${yyyy}-${MM}-${dd}`;
+  return formatted_date;
+};
